f984545a3640f24df3b0ce341178a74f35da83ac
beatho <38127543+beatho@users.noreply.github.com>
2019-09-30 15:13:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Parcours des anciens</title>
+    <link rel="icon" href="images/logo.png">
+    <meta name="generator" content="Geany 1.32" />
+    <link rel="stylesheet" href="style.css">
+    <script src="functions.js"></script>
+</head>
+<body>
+
+<div class="content_tabs">
+    <div class="content_tab" id="content_tab_home" style="display: block">
+        <img src="images/ENS-Rennes-105_carroussel.jpg">
+        <h2>Bienvenue sur le site des anciens élèves de l'ENS Rennes.</h2>
+        <p>
+            Ce site a pour but de réunir d'anciens élèves et de garder contact
+            avec l'école. (Blabla au choix)
+        </p>
+    </div>
+    <div class="content_tab" id="content_tab_students">
+        <div>
+            coucou
+        </div>
+    </div>
+    <div class="content_tab" id="content_tab_connexion">
+    <div class="content_tab2" id="content_tab2_Session">
+        <div id ="formulaire">
+            <span style = "text-align:center" >
+                <form method="post" action= "connexion.php" id = "sign_in" style="display:inline-block">
+                    <div class ="form">
+                        <h3> Pseudo :</h3>
+                        <input type="text" name="pseudo" id="pseudo" placeholder="Ex : Zozor" size="20" maxlength="10" required /> <br />
+                    </div>  
+                    <div class ="form">
+                        <h3> Password :</h3>
+                        <input type="password" name="pass" id="pass" required  />
+                    </div> 
+                    <p class = form>
+                        <input type="submit" value="Connexion" id = "button_co" />
+                    </p>
+                </form>	
+            </span> 
+            <span style = "text-align:center" >
+                <form method="post" action= "../php/subscribe.php" style="display:inline-block">
+                    <div class ="form">
+                        <h3> Pseudo :</h3>
+                        <input type="text" name="pseudo" id="pseudo" placeholder="Ex : Zozor" size="20" maxlength="10" required /> <br />
+                    </div>  
+                    <div class ="form">
+                        <h3> Password :</h3>
+                        <input type="password" name="pass" id="pass" required title='6 cractères au minimum' />
+                    </div> 
+                    <p class = form>
+                        <input type="submit" value="Inscription" id = "button_co" />
+                    </p>
+                    
+                </form>	
+            </span>  
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
