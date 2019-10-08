a01807314c50fa7abf32407fc37f6bbc2fe224a3
beatho <beatrice.thomas@ens-rennes.fr>
2019-10-08 08:54:49 +0200

    changement creation formulaire

--- formulaire.php
+++ formulaire.php
@@ -3,13 +3,63 @@ if (isset($_GET['err'])){
     $errID = htmlspecialchars(trim($_GET['err']));
     echo "<div class='warning'> $errID </div>";
 }
-?>
 
+$feat_co = array(
+    
+    "E_mail" => ["email", "mail","prenom.nom@ens-rennes.fr", "44"],
+    "Mot de passe" => ["password", "pass","******", "36"],
+    "button" => ["submit", "Connexion","button_co"]);
+
+$feat_sub = array(
+    "Nom" => ["text", "nom","Ex : Legrand", "46"],
+    "Prénom" => ["text", "prenom","Ex : Zozor", "42"],
+    "Mot de passe" => ["password", "pass","******", "36"],
+    "E_mail" => ["email", "mail","prenom.nom@ens-rennes.fr", "44"],
+    "Promo" => ["number", "promo","2016", "1980"],
+    "button" => ["submit", "Inscription","button_sub"]);
+    
+?>
 <div id ="formulaires">
-    <form class="wrap_form" method="post" action= "connexion.php" id = "sign_in" style="display:inline-block">
+    <form class="wrap_form" method="post" action= "connexion.php"  >
+        <?php
+            foreach($feat_co as $key => $values){
+                echo "<div class ='form'>";  
+                    if ($key == "button"){
+                        echo "<input type='$values[0]' name='$values[1]' id='$values[2]' />";
+                    } else {
+                        echo "<h3> $key : ";
+                        echo "<input type='$values[0]' name='$values[1]' id='$values[1]' placeholder='$values[2]' size='$values[3]' required /> <br />";
+                        echo "</h3>";
+                    }
+                echo "</div>"; 
+            }
+        ?>
+    </form>	
+    <form class="wrap_form" method="post" action= "inscription.php" >
+        <?php
+            foreach ($feat_sub as $key => $values){
+                echo "<div class ='form'>";  
+                    if ($key == "button"){
+                        echo "<input type='$values[0]' name='$values[1]' id='$values[2]' />";
+                    } else if ($key == "Promo")  {
+                        echo "<h3> $key : ";
+                        echo "<input type='$values[0]' name='$values[1]' id='$values[1]' placeholder='$values[2]' min='$values[3]' required /> <br />";
+                        echo "</h3>";
+                    } else {
+                        echo "<h3> $key : ";
+                        echo "<input type='$values[0]' name='$values[1]' id='$values[1]' placeholder='$values[2]' size='$values[3]' required /> <br />";
+                        echo "</h3>";
+                    }
+                echo "</div>"; 
+            }
+        ?>
+    </form>	
+
+<!--<div id ="formulaires">
+    <form class="wrap_form" method="post" action= "connexion.php" id = "sign_in" >
         <div class ="form">
             <h3> E-mail :
-                <input type="email" name="mail" id="mail" placeholder="prenom.nom@ens-rennes.fr" size="44" maxlength="50" required /> <br />
+                <input type="email" name="mail" id="mail" placeholder="prenom.nom@ens-rennes.fr" size="44" required /> <br />
             </h3>
             
         </div>  
@@ -22,7 +72,7 @@ if (isset($_GET['err'])){
             <input type="submit" value="Connexion" id = "button_co" />
         </div>
     </form>	
-    <form class="wrap_form" method="post" action= "inscription.php" style="display:inline-block">
+    <form class="wrap_form" method="post" action= "inscription.php" >
         <div class ="form">
             <h3> Nom :
                 <input type="text" name="nom" id="nom" placeholder="Ex : Legrand" size="46" maxlength="10" required /> <br />
@@ -45,11 +95,11 @@ if (isset($_GET['err'])){
         </div>  
         <div class ="form">
             <h3>Promo :
-                <input type="number" name="promo" id="promo" placeholder="2016" maxlength="10" min="1980" defalut="1990" required /> <br />
+                <input type="number" name="promo" id="promo" placeholder="2016" maxlength="10" min="1980" default="1990" required /> <br />
             </h3>
         </div>  
         <div class = form>
-            <input type="submit" value="Inscription" id = "button_co" />
+            <input type="submit" value="Inscription" id = "button_sub" />
         </div>
     </form>
-</div>
+</div>-->
